392d9899cd470e8348403ffcdcca314d7b02cc0d
Joe Wilner <jhwilner1@gmail.com>
2013-10-06 16:43:12 -0400

    Initial

---
+++ test/joy_exercises/core_test.clj
@@ -0,0 +1,7 @@
+(ns joy-exercises.core-test
+  (:require [clojure.test :refer :all]
+            [joy-exercises.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
